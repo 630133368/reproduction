8b8f018d6c3e4c1a3a69f784a295b77ca3bf5b97
董贤斌 <630133368@qq.com>
2019-05-08 20:39:07 +0800

    To reproduce the BUG

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <p>This is about</p>
+    <p>I can cache both development and production environments</p>
+    <input type="text"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class About extends Vue {
+  constructor () {
+    super()
+    console.log(this.$options.name)
+    const _this: any = this
+    _this.$options.__proto__.name = 'About'
+    console.log(this.$options.name)
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>This is Home</p>
+    <p>Development environment I can cache</p>
+    <input type="text"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Home extends Vue {
+  private created() {
+    console.log(this.$options.name);
+  }
+}
+</script>
